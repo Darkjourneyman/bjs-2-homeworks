3a5c882b869e8e09b707f393f716fbdc691e1528
Anton Evseev <darkjourneyman@yandex.ru>
2025-02-19 18:09:43 +0400

    Home Task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		let arr = [];
+		return arr;
+	} else if (d === 0) {
+		let arr = [-b / (2 * a)];
+		return arr;
+	} else {
+		let arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentPerMonth = percent / 12 / 100;
+	let principalAmount = amount - contribution;
+	let paymentPerMonth = principalAmount * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
+	let totalAmount = paymentPerMonth * countMonths;
+	return Number(totalAmount.toFixed(2));
 }
