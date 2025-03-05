7a5a63e91a664803a8ea57afa0b0c59c0d105a89
Anton Evseev <darkjourneyman@yandex.ru>
2025-03-05 12:22:11 +0400

    Home Task Completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.hasOwnProperty("marks")) {
+		this.marks.push(...marksToAdd);
+	};
+
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.hasOwnProperty("marks") || !Array.isArray(this.marks)) {
+		return 0;
+	} else if (this.marks.length === 0) {
+		return 0;
+	}
+	return (this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0)) / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
